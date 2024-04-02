c52ad5fe97b2052e4f9269faab0c48274c02249f
Shehab Diab <shehabgehaddiab@gmail.com>
2024-04-02 22:50:20 +0200

    modify test cases

--- test.py
+++ test.py
@@ -50,22 +50,6 @@ print()
 # all y exists x -C -> D & father(A, B)
 # expr = parse_from_str('-(-a & b -> c) ')
 
-expr = parse_from_str('exists x -P(x) -> (exists y all z P(x, y, z, x) & -Q(x, y))')
-print(expr)
-
-new_expr = Resolution(expr)
-new_expr = new_expr.apply_all_steps(True)
-
-print('--------------------------------------------------------------------------------\n\n')
-
-expr = parse_from_str("exists x -P(x) -> (exists y all z P(x, y, z, x) & -Q(x, y))")
-r = Resolution(expr).apply_all_steps()
-print(expr.str(True))
-print(expr.str(False))
-
-dot = Resolution(expr).visualize_tree(expr)
-dot.render(outfile='tree.png')
-
 # print("after steps: ", r)
 # print("clauses = ", [[str(y) for y in x] for x in r.clauses])
 
@@ -95,7 +79,7 @@ dot.render(outfile='tree.png')
 # print("after steps: ", r)
 # print("clauses = ", [[str(y) for y in x] for x in r.clauses])
 
-expr = parse_from_str('exists x -P(x) -> (all x exists y P(x, y) & Q(x, y))')
+expr = parse_from_str('exists x -P(x) -> (exists y all z P(x, y, z, x) & -Q(x, y))')
 print(expr)
 
 new_expr = Resolution(expr)
@@ -107,3 +91,11 @@ expr = parse_from_str("a & (b | c & d) | e ")
 r = Resolution(expr).conjunctive_form()
 print("expr: ", r.expression.str(True))
 
+
+expr = parse_from_str("exists x -P(x) -> (exists y all z P(x, y, z, x) & -Q(x, y))")
+r = Resolution(expr).apply_all_steps()
+print(expr.str(True))
+print(expr.str(False))
+
+dot = Resolution(expr).visualize_tree(expr)
+dot.render(outfile='tree.png')
